5f5fa54f94b6f25d11a4a9142e72cfd54384b797
P <cinno_canno_@hotmail.com>
2021-01-23 21:05:31 +0300

    ders 4 sonu

---
+++ Metotlar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Aciklama = "Amasya Elması";
+            urun1.Fiyati = 15;
+            urun1.StokAdedi = 5;
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+            urun2.Fiyati = 80;
+            urun2.StokAdedi = 8;
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.StokAdedi);
+                Console.WriteLine("----------------");
+            }
+            Console.WriteLine("-------METOTLAR--------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            
+        }
+    }
+}
