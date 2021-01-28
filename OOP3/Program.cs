521c0c218b555acf784a67984f7b5eed60fc8e59
P <cinno_canno_@hotmail.com>
2021-01-28 04:31:24 +0300

    5. sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtıyacKrediManager();
+            
+            
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            
+            
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+            
+            ILoggerService smsLoggerService = new SmsLoggerService();
+            ILoggerService dataBaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLogerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager,smsLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager ,tasitKrediManager};
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+          
+        }
+    }
+}
